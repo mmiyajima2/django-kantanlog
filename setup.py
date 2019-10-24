271e076156d4d722f1592c9eee6f65b7b67fb090
MIYAJIMA,Masafumi <mmiyajima2@gmail.com>
2019-10-24 06:26:59 +0000

    Testing for CI->PyPI

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ os.chdir(
 
 setup(
     name='djangokantanlog',
-    version='0.0.1.beta',
+    version='0.0.2.beta',
     packages=find_packages(),
     lincense='MIT',
     description='Django helper app for logging ID.',
